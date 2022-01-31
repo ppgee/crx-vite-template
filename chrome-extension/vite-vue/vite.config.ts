916f838123662d478c7596d6d11dc01dcbd72dbb
PGee <283959070@qq.com>
2022-01-31 17:24:55 +0800

    修改：新增vite构建chrome extension模板

---
+++ chrome-extension/vite-vue/vite.config.ts
@@ -0,0 +1,57 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './src/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        /**
+         * 点击插件图标出现的弹窗
+         */
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        background: resolve(__dirname, 'src/background/main.ts')
+      },
+      output: {
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: '[name]/index.js',
+        assetFileNames: 'assets/[ext]/[name].[ext]'
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+    copy({
+      verbose: true,
+      hook: 'writeBundle',
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: 'dist'
+        },
+        {
+          src: 'public/icons/*',
+          dest: 'dist/assets/icons/'
+        }
+      ]
+    })
+  ]
+})
