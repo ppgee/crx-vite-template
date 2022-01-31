3dcdd4a4638e6a7dd573f14d99fcb558be2cc654
PGee <283959070@qq.com>
2022-01-31 19:55:39 +0800

    修改：新增vite-vue-ts模板更多明细

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # new-project-templates
-provide some templates for creating new projects
+提供基于Vite + Vue3 + Typescript的Chrome Extension模板
+## 使用模板
+```bash
+yarn install
+
+# 开发命令
+yarn dev
+
+# 构建命令
+yarn build
+```
+
+## 结合Chrome调试
+当第一次跑完以上命令后，找到Chrome的 *扩展程序* 点击 *加载已解压的扩展程序* 并指向该项目的 *dist* 文件夹即可
--- chrome-extension/vite-vue-ts/vite.config.ts
+++ chrome-extension/vite-vue-ts/vite.config.ts
@@ -2,9 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from 'rollup-plugin-copy'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +13,7 @@ export default defineConfig({
   },
   build: {
     outDir: '../dist',
+    emptyOutDir: false,
     rollupOptions: {
       input: {
         /**
@@ -33,12 +31,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
     copy({
       verbose: true,
       hook: 'writeBundle',
